c42b0675fbc5c6b4ca620893be7dbca9f9afe473
Amil Rustamov <amil93.rustamov@gmail.com>
2021-11-24 12:05:38 +0400

    finished course

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const Joi = require("joi");
+//installed for to validation (making a schema and compare)
 const app = express();
 
+app.use(express.json());
 const courses = [
   { id: 1, name: "javascript" },
   { id: 2, name: "react js" },
-];
+];0
 
 app.get("/", (req, res) => {
   res.send("hello world");
@@ -16,19 +19,56 @@ app.get("/api/courses", (req, res) => {
 
 app.get("/api/courses/:ID", (req, res) => {
   let amil = req.params.ID;
-  const course=courses.find(item=>parseInt(item.id)===parseInt(amil));
-  if(!course) res.status(404).send('course not found') 
+  const course = courses.find((item) => parseInt(item.id) === parseInt(amil));
+  if (!course) res.status(404).send("course not found");
   res.send(course);
 });
 
-app.post('/api/courses',(req,res)=>{
-    const course={
-         id:3,
-         name:'flutter' 
-    }
-    courses.push(course)
-    res.send(course);
+app.post("/api/courses", (req, res) => {
+  // const schema = {
+  //   name: Joi.string().min(3).required(),
+  // };
+  // const result = schema.validate(req.body);
+  const{error}=validationFunc(req.body) ;
+  if(error){
+    return res.status(404).send(error.details[0].message)
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put('/api/courses/:ID',(req,res)=>{
+  const course = courses.find((item) => parseInt(item.id) === parseInt(req.params.ID));
+  if (!course) res.status(404).send("course not found");
+     
+  const{error}=validationFunc(req.body) ;
+  
+  if(error){
+    return res.status(404).send(error.details[0].message)
+  }
+
+  course.name=req.body.name
+  res.send(course);
 })
+
+app.delete('/api/courses/:ID',(req,res)=>{
+
+   const index=courses.indexOf(req.params.ID)
+   if(!index) return res.send('corse not found ') ;
+   const course=courses[parseInt(req.params.ID)-1];
+   courses.splice(index,1)
+   res.send(course)
+})
+
+
+const validationFunc=(reqBody)=>{
+  const schema = Joi.object({ name: Joi.string() .min(3) .required() });
+  return schema.validate(reqBody);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
